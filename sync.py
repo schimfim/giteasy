af4590c5765cbb7ff2911c9d038154efddb6eaec
uncleWalter <schimfim@mac.com>
2013-08-02 13:45:17 -0700

    Auto commit by giteasy

--- sync.py
+++ sync.py
@@ -1,4 +1,4 @@
 from giteasy import RepoCLI
 
-files = ['agithub.py', 'giteasy.py', 'git_test.py', 'sync.py']
+files = ['agithub.py', 'giteasy.py', 'git_test.py', 'sync.py', '__init__.py']
 RepoCLI('giteasy', 'schimfim', 'Ninz2009', files)
